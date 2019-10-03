c58dcee52286478f90c0a558c6c873d1feef9b86
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-10-03 20:20:02 -0300

    add parameters to device

--- src/main/kotlin/com/dataner/application/web/entities/Device.kt
+++ src/main/kotlin/com/dataner/application/web/entities/Device.kt
@@ -3,9 +3,15 @@ package com.dataner.application.web.entities
 import com.dataner.domain.devices.entities.Device
 
 data class Device (
-    val deviceId: String
+    val deviceId: String,
+    val deviceDescription: String,
+    val environmentId: String,
+    val tagId: List<Int>
 ) {
     fun toDevice() = Device (
-        deviceId = deviceId
+        deviceId = deviceId,
+        deviceDescription = deviceDescription,
+        environmentId = environmentId,
+        tagId = tagId
     )
 }
