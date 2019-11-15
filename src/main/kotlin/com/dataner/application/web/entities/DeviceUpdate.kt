4ca81478f195ce96514af6221bc3b912d1cc24f2
ArielMKT <arielmagalhaeslira@gmail.com>
2019-11-15 19:10:39 -0200

    add deviceState on data class to request

--- src/main/kotlin/com/dataner/application/web/entities/DeviceUpdate.kt
+++ src/main/kotlin/com/dataner/application/web/entities/DeviceUpdate.kt
@@ -6,6 +6,7 @@ data class DeviceUpdate (
     val deviceId: String,
     val deviceIdUpdate: String = deviceId,
     val deviceDescription: String,
+    val deviceState: Boolean = false,
     val deviceType: String,
     val tagId: List<Int>? = null,
     val workplaceId: Int
@@ -14,6 +15,7 @@ data class DeviceUpdate (
         deviceId = deviceId,
         deviceIdUpdate = deviceIdUpdate,
         deviceDescription = deviceDescription,
+        deviceState = deviceState,
         deviceType = deviceType,
         tagId = tagId,
         workplaceId = workplaceId
--- src/main/kotlin/com/dataner/domain/devices/entities/DeviceUpdate.kt
+++ src/main/kotlin/com/dataner/domain/devices/entities/DeviceUpdate.kt
@@ -4,7 +4,7 @@ data class DeviceUpdate (
     val deviceId: String,
     val deviceIdUpdate: String,
     val deviceDescription: String,
-    val deviceState: Boolean = false,
+    val deviceState: Boolean,
     val deviceType: String,
     val tagId: List<Int>?,
     val workplaceId: Int
